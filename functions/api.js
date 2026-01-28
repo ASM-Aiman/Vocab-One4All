8a14d4aed51a09e8bc55738f42efc974e6c15016
Aiman <elrudoasm@gmail.com>
2026-01-28 16:27:26 +0530

    pre-deploy changes-4, libresql

--- functions/api.js
+++ functions/api.js
@@ -1,4 +1,4 @@
-const { createClient } = require("@libsql/client");
+import { createClient } from '@libsql/client/web';
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
